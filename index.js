67665c540c0bd21357c4dc0753c37dd6a3a7fbd6
Jeff Valore <rally25rs@yahoo.com>
2017-11-14 11:11:43 -0500

    support commonjs modules

--- index.js
+++ index.js
@@ -1,85 +1,99 @@
-Backbone.ForkableModel = (function(_) {
-  'use strict';
-
-  function fork() {
-    var clone = this.clone();
-    clone._forkedFrom = this;
-    return clone;
+(function() {
+  if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    // commonjs
+    var _ = require('underscore');
+    var Backbone = require('backbone');
+    module.exports = factory(Backbone, _);
+  } else if(window._ && window.Backbone) {
+    // global
+    window.Backbone.ForkableModel = factory(window.Backbone, window._);
+  } else {
+    console.error('Unable to determine module system. Requires CJS or have global Backbone and _ (underscore) defined.');
   }
 
-  function unfork() {
-    var forkedFrom = this._forkedFrom;
-    var removedKeys = _.difference(forkedFrom.keys(), this.keys());
-    var unsetProps = {};
+  function factory(Backbone, _) {
+    'use strict';
 
-    _.each(removedKeys, function(key) {
-      unsetProps[key] = undefined;
-    });
+    function fork() {
+      var clone = this.clone();
+      clone._forkedFrom = this;
+      return clone;
+    }
 
-    forkedFrom.set(this.attributes);
-    forkedFrom.unset(unsetProps);
+    function unfork() {
+      var forkedFrom = this._forkedFrom;
+      var removedKeys = _.difference(forkedFrom.keys(), this.keys());
+      var unsetProps = {};
 
-    return this._forkedFrom;
-  }
+      _.each(removedKeys, function(key) {
+        unsetProps[key] = undefined;
+      });
 
-  function forkHasChanges() {
-    var model1 = this._forkedFrom;
-    var model2 = this;
+      forkedFrom.set(this.attributes);
+      forkedFrom.unset(unsetProps);
 
-    return _objectHasDifferences.call(this, model1, model2);
-  }
+      return this._forkedFrom;
+    }
 
-  function _objectHasDifferences(obj1, obj2) {
-    var i;
-    var key;
-    var keys1;
-    var keys2;
+    function forkHasChanges() {
+      var model1 = this._forkedFrom;
+      var model2 = this;
 
-    if(this._forkedFrom === undefined) {
+      return _objectHasDifferences.call(this, model1, model2);
+    }
+
+    function _objectHasDifferences(obj1, obj2) {
+      var i;
+      var key;
+      var keys1;
+      var keys2;
+
+      if(this._forkedFrom === undefined) {
+        return false;
+      }
+
+      if(obj1 instanceof Backbone.Model) {
+        obj1 = obj1.attributes;
+      }
+      if(obj2 instanceof Backbone.Model) {
+        obj2 = obj2.attributes;
+      }
+
+      keys1 = _.keys(obj1);
+      keys2 = _.keys(obj2);
+
+      if(keys1.length !== keys2.length) {
+        return true;
+      }
+      if (_.difference(keys1, keys2).length > 0) {
+        return true;
+      }
+      if (_.difference(keys2, keys1).length > 0) {
+        return true;
+      }
+
+      for(i = 0; i < keys1.length; i++) {
+        key = keys1[i];
+        if(typeof(obj1[key]) === 'object') {
+          if(_objectHasDifferences.call(this, obj1[key], obj2[key])) {
+            return true;
+          }
+        } else {
+          if(obj1[key] !== obj2[key]) {
+            return true;
+          }
+        }
+      }
+
       return false;
     }
 
-    if(obj1 instanceof Backbone.Model) {
-      obj1 = obj1.attributes;
-    }
-    if(obj2 instanceof Backbone.Model) {
-      obj2 = obj2.attributes;
-    }
-
-    keys1 = _.keys(obj1);
-    keys2 = _.keys(obj2);
-
-    if(keys1.length !== keys2.length) {
-      return true;
-    }
-    if (_.difference(keys1, keys2).length > 0) {
-      return true;
-    }
-    if (_.difference(keys2, keys1).length > 0) {
-      return true;
-    }
-
-    for(i = 0; i < keys1.length; i++) {
-      key = keys1[i];
-      if(typeof(obj1[key]) === 'object') {
-        if(_objectHasDifferences.call(this, obj1[key], obj2[key])) {
-          return true;
-        }
-      } else {
-        if(obj1[key] !== obj2[key]) {
-          return true;
-        }
+    return {
+      mixin: {
+        fork: fork,
+        unfork: unfork,
+        forkHasChanges: forkHasChanges
       }
-    }
-
-    return false;
-  }
-
-  return {
-    mixin: {
-      fork: fork,
-      unfork: unfork,
-      forkHasChanges: forkHasChanges
-    }
+    };
   };
-})(window._);
+})();
